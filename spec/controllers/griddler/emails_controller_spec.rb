21dbb014ae9ee38be2407b9d0c211a3eee24a53c
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-02-04 22:21:16 -0500

    Whitespace and tab formatting

--- spec/controllers/griddler/emails_controller_spec.rb
+++ spec/controllers/griddler/emails_controller_spec.rb
@@ -2,34 +2,34 @@ require 'spec_helper'
 
 describe Griddler::EmailsController do
   describe 'POST create' do
-    it 'is successfull' do
-        post :create, email_params
-        response.should be_success
+    it 'is successful' do
+      post :create, email_params
+
+      response.should be_success
     end
 
     it 'creates a new Griddler::Email' do
-        controller.stub(:params).and_return({})
-        email = double(process: 'something')
-        Griddler::Email.stub(:new).and_return(email)
-        Griddler::Email.should_receive(:new).with({})
-        email.should_receive(:process)
+      controller.stub(:params).and_return({})
+      email = double(process: 'something')
+      Griddler::Email.stub(:new).and_return(email)
+      Griddler::Email.should_receive(:new).with({})
+      email.should_receive(:process)
 
-        post :create
+      post :create
     end
   end
-end
 
-private
 
-def email_params
-  {
-    headers: 'Received: by 127.0.0.1 with SMTP...',
-    to: 'thoughtbot <tb@example.com>',
-    from: 'John Doe <someone@example.com>',
-    subject: 'hello there',
-    text: 'this is an email message',
-    html: '<p>this is an email message</p>',
-    charsets: '{"to":"UTF-8","html":"ISO-8859-1","subject":"UTF-8","from":"UTF-8","text":"ISO-8859-1"}',
-    SPF: "pass"
-  }
+  def email_params
+    {
+      headers: 'Received: by 127.0.0.1 with SMTP...',
+      to: 'thoughtbot <tb@example.com>',
+      from: 'John Doe <someone@example.com>',
+      subject: 'hello there',
+      text: 'this is an email message',
+      html: '<p>this is an email message</p>',
+      charsets: '{"to":"UTF-8","html":"ISO-8859-1","subject":"UTF-8","from":"UTF-8","text":"ISO-8859-1"}',
+      SPF: "pass"
+    }
+  end
 end
